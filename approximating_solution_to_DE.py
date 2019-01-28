11706ab8fedb3d469ff406b0fc4f10e5bc477db7
Ife Ademolu-Odeneye <ife.ademoluodeneye@gmail.com>
2019-01-28 11:29:03 -0500

    Update approximating_solution_to_DE.py

--- approximating_solution_to_DE.py
+++ approximating_solution_to_DE.py
@@ -1,4 +1,5 @@
 import matplotlib,pyplot as plt
+
 delta_t=2
 pi=3.1415926
 t=[0]
@@ -13,14 +14,14 @@ phi_s=[]
 phi_s_d=[]
 phi_s_dd=[]
 
-delta_phi=[phi_b[0]-phi_b[0]]
+delta_phi=[phi_b[0]-phi_s[0]]
 
 range=[]
 
 a_s = 5 #function in terms of l1 and l2
 a_b = 5 #function in terms of l1 and l2
-w_s = 5 #function in terms of l1 and l2
-w_b = 5 #function in terms of l1 and l2
+w_s_squared = 5 #function in terms of l1 and l2
+w_b_squared = 5 #function in terms of l1 and l2
 
 def euler_step(last_var: float, last_var_d : float):
     var = last_var + last_var_d * delta_t
@@ -34,15 +35,15 @@ def calculate_phi_b_dd(current_phi_b, current_phi_b_d, current_phi_s, current_ph
                                                                              + (a_s *
                                                                                 current_phi_b_d ** 2 *
                                                                                 sin(
-                                                                                    -current_delta_phi) - w_s ** 2 * sin(
-                        current_phi_s)) * cos(current_delta_phi) - w_b ** 2 * sin(current_phi_b)))
+                                                                                    -current_delta_phi) - w_s_squared * sin(
+                        current_phi_s)) * cos(current_delta_phi) - w_b_squared * sin(current_phi_b)))
     return new_phi_b_dd
 
 
 def calculate_phi_s_dd(current_phi_b_d, current_phi_s,
                        current_phi_b_dd, current_delta_phi):
     new_phi_s_dd = a_s * (current_phi_b_d ** 2 * sin(-current_delta_phi) + current_phi_b_dd * cos(
-        -current_delta_phi)) - w_s ** 2 * sin(current_phi_s)
+        -current_delta_phi)) - w_s_squared * sin(current_phi_s)
     return new_phi_s_dd
 
 def calculate_range(current_phi_s, current_phi_s_d):
@@ -50,17 +51,6 @@ def calculate_range(current_phi_s, current_phi_s_d):
     new_range= current_phi_s+current_phi_s_d
 
     return new_range
-
-def set_initial_conditions:
-    phi_b.append(3 * pi / 4)
-    phi_b_d.append(0.0)
-    phi_s.append(pi / 2)
-    phi_s_d .append(0.0)
-    phi_b_dd.append(calculate_phi_b_dd(phi_b[0], phi_b_d[0], phi_s[0], phi_s_d[0]))
-    phi_s_dd.append(calculate_phi_s_dd(phi_b[0], phi_b_d[0],phi_s[0],phi_s_d[0], phi_b_dd[0]))
-    delta_phi.append([phi_b[0] - phi_b[0]])
-    range.append(0)
-
 
 
 def stepequation("takes in the last term of each eqn"):
@@ -74,7 +64,19 @@ def stepequation("takes in the last term of each eqn"):
     range.append(calculate_range(phi_s[i], phi_s_d[i]))
 
 def set_constants(l1,l2):
-    
+    phi_b.append(3 * pi / 4)
+    phi_b_d.append(0.0)
+    phi_s.append(pi / 2)
+    phi_s_d .append(0.0)
+    phi_b_dd.append(calculate_phi_b_dd(phi_b[0], phi_b_d[0], phi_s[0], phi_s_d[0]))
+    phi_s_dd.append(calculate_phi_s_dd(phi_b[0], phi_b_d[0],phi_s[0],phi_s_d[0], phi_b_dd[0]))
+    delta_phi.append([phi_b[0] - phi_b[0]])
+    range.append(0)
+    a_s = l2/ls  # function in terms of l1 and l2
+    a_b = m2*l2*ls / (m1*l1**2 + m2*l2**2 + mb*lb**2 + Ib)  # function in terms of l1 and l2
+    w_s_squared = g/ls  # function in terms of l1 and l2
+    w_b_squared = g*(m1*l1 - m2*l2 - mb*lb)/(m1*(l1**2) + m2*(l2**2) + mb*(lb**2) +Ib)  # function
+    # in terms of l1 and l2
 
 
 
@@ -82,5 +84,5 @@ def optimize_this(l1, l2):
     stepequation()
     while range[-1]<range[-2]:
         stepequation()
-
     plt.plot(t,range)
+    return range[-1]
